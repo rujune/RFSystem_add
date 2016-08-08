491a7d355f7f25d89102760bb98bcb89ecb8a936
wen <shbowm@gmail.com>
2016-08-08 21:44:39 +0800

    DAO Imp 0.05a

--- app/src/main/java/com/wen/rfsystem/SFsysDAOImp.java
+++ app/src/main/java/com/wen/rfsystem/SFsysDAOImp.java
@@ -1,5 +1,10 @@
 package com.wen.rfsystem;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -7,19 +12,41 @@ import java.util.List;
  */
 public class SFsysDAOImp implements SFsysDAO{
 
+    SQLiteDatabase db;
+
 
     @Override
-    public void sqladd(customer person) {
+    public void cusadd(customer person) {
+
+        ContentValues cv = new ContentValues();
+        cv.put("name",  person.name);
+        cv.put("addr", person.address);
+        cv.put("tel", person.tel);
+        long id = db.insert("student", null, cv);// 執行SQL 語句
+    }
+
+    @Override
+    public void cusdel(int id) {
+        db.execSQL("Delete from student where name='" + id + "'");
+    }
+
+    @Override
+    public void cusupdata(int id) {
+        db.execSQL("Update student set addr = '" + id + "' ,tel='" + id + "' Where name='" + id + "'");
+    }
+
+    @Override
+    public void resadd(reserve reserve) {
 
     }
 
     @Override
-    public void sqldel(int id) {
+    public void resdel(int id) {
 
     }
 
     @Override
-    public void sqlupdata(int id) {
+    public void resupdata(int id) {
 
     }
 
@@ -34,7 +61,25 @@ public class SFsysDAOImp implements SFsysDAO{
     }
 
     @Override
-    public List getAllStudent() {
+    public List getAllcustomer() {
+        ArrayList<customer> mylist = new ArrayList<>();
+
+        Cursor c = db.rawQuery("Select * from student", null);
+        if (c.moveToFirst())
+        {
+            do {
+                customer t = new customer();
+                mylist.add(t);
+            } while (c.moveToNext());
+        }
+
+        return mylist;
+    }
+
+    @Override
+    public List getreserve(String day) {
         return null;
     }
+
+
 }
